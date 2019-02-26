3a906a7bf8592a2e7944befea1a2547000856ebb
Eduardo Naveda <eddnav@gmail.com>
2019-02-25 23:54:07 -0400

    Demo showing a strange blink in AAC navigation UI 1.0.0-rc01
    After navigating to an nested graph destination, then navigating to another in the non nested graph, there's a strange blink with the transitions that shows the content of the fragment exiting for a short moment

---
+++ app/src/main/java/com/eddnav/transitionbug/FragmentHost.kt
@@ -0,0 +1,46 @@
+package com.eddnav.transitionbug
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.fragment_host.*
+
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ *
+ */
+class FragmentHost : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_host, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        fab.setOnClickListener {
+            findNavController().navigate(R.id.action_fragmentHost_to_extraFragment)
+        }
+
+        val view : View = view.findViewById(R.id.bottom_navigation_host)
+        val navController = findNavController(view)
+
+        navigation.setupWithNavController(navController)
+    }
+
+}
